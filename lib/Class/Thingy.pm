1451b7b2ae2865dca0035156d2d647bed7eaeb7d
Darren Embry <dse@webonastick.com>
2018-08-09 21:48:29 -0400

    fixes

--- lib/Class/Thingy.pm
+++ lib/Class/Thingy.pm
@@ -21,11 +21,13 @@ sub import {
     my $package = caller;
     push(@PACKAGES_USING_ME, $package);
 
-    # In case of any runtime imports of Class::Thingy.  We don't need
-    # to check ${^GLOBAL_PHASE} because add_superclass_to_packages is
-    # idempotent.
-    add_superclass_to_packages();
+    # In case of any imports of Class::Thingy after the BEGIN phase
+    # (typically runtime).
+    if (grep { ${^GLOBAL_PHASE} eq $_ } qw(INIT RUN END DESTRUCT)) {
+        add_superclass_to_packages();
+    }
 
+    # Go to the original import method.
     my $super_import = $package->can("SUPER::import");
     goto &$super_import if $super_import;
 }
@@ -34,7 +36,8 @@ sub add_superclass_to_packages {
     # We add Class::Thingy::Object as a superclass of any packages
     # that don't have us listed as a superclass already.  We append it
     # so any "new" methods added afterwards get favored by the default
-    # method resolution order.
+    # method resolution order.  If C::T::O is already in each
+    # package's @ISA list, nothing is changed.
     foreach my $package (@PACKAGES_USING_ME) {
         no strict "refs";
         my $isa_var_name = "${package}::ISA";
