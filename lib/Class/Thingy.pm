029c60a12ed7ec8fe6fcd0277ad2f50d46c4cfc7
Darren Embry <dse@webonastick.com>
2018-08-09 21:39:40 -0400

    comment about Class::Tiny features and what we do instead

--- lib/Class/Thingy.pm
+++ lib/Class/Thingy.pm
@@ -58,4 +58,27 @@ sub public (*;@) {
     *{$sub_name} = $sub;
 }
 
+# Features of Class::Tiny::
+# - defines attributes via import arguments
+#   - we export a method called public for creating attributes
+#   - we call attributes "properties" instead
+# - generates read-write accessors
+#   - we do this
+# - supports lazy attribute defaults
+#   - in the pipeline
+# - supports custom accessors
+#   - eh?
+# - superclass provides a standard new constructor
+#   - sure, why not, we do it, base class is Class::Thingy::Object
+# - new takes a hash reference or list of key/value pairs
+#   - sure, why not, Class::Thingy::Object::new does this
+# - new supports providing BUILDARGS to customize constructor options
+#   - eh?
+# - new calls BUILD for each class from parent to child
+#   - useless feature imo
+# - superclass provides a DESTROY method
+#   - useless feature imo
+# - DESTROY calls DEMOLISH for each class from child to parent
+#   - useless feature imo
+
 1;
